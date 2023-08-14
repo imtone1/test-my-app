3c08b7f1a3770b5a494a4ba2d9916c7c116240bf
sapair <88165529+imtone1@users.noreply.github.com>
2023-08-15 00:43:52 +0300

    metadata title tag changed

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Test my app',
+  title: 'Test my app - website for testing',
   description: 'App for frontend testing',
 }
 
